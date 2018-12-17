2c3ba10892689550edc691c73b1a2701a7e99bf8
AIM1993 <244018719@qq.com>
2018-12-17 10:54:18 +0800

    引入nodemon

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var connection = require('./db/connection');
 
 
 var indexRouter = require('./routes/index');
@@ -10,32 +9,12 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-var db_data = [];
-
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-connection.connect(function(err) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log('Connect success');
-});
-
-connection.query('SELECT * FROM `testuser`', function (error, results, fields) {
-    if (error) throw error;
-    db_data = results;
-    console.log('Query data...');
-});
-
-connection.end();
 
 module.exports = app;
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
+var connection = require('../db/connection');
+
+var db_data = [];
+// connecting database
+connection.connect(function(err) {
+  if (err) {
+      console.log(err);
+      return;
+  }
+  console.log('Connect success');
+});
+
+
+connection.query('SELECT * FROM `testuser`', function (error, results, fields) {
+  if (error) throw error;
+  db_data = results;
+  console.log("Query Success...")
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.json({result: db_data});
 });
 
+
+connection.end();
 module.exports = router;
