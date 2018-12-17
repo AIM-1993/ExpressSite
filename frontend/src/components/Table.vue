0dd6a7f72f1519fa8b282e122ba8ef03f8d21b8c
AIM1993 <244018719@qq.com>
2018-12-17 12:00:27 +0800

    实现了前后端数据传输

--- frontend/src/components/Table.vue
+++ frontend/src/components/Table.vue
@@ -3,30 +3,18 @@
     <table class="table">
       <thead class="thead">
         <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
+          <th scope="col">Id</th>
+          <th scope="col">Username</th>
+          <th scope="col">Score</th>
+          <th scope="col">Age</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td>Larry</td>
-          <td>the Bird</td>
-          <td>@twitter</td>
+        <tr v-for="(data, index) in tableData" :key="index">
+          <th scope="row">{{data.id}}</th>
+          <td>{{ data.username }}</td>
+          <td>{{ data.score }}</td>
+          <td>{{ data.age }}</td>
         </tr>
       </tbody>
     </table>
@@ -39,6 +27,22 @@ export default {
   name: 'Table',
   props: {
     msg: String
+  },
+  data () {
+    return {
+      tableData: ""
+    }
+  },
+  created: function () {
+    this.loadData();
+  },
+  methods: {
+    loadData: function () {
+      this.$ajax.get('http://localhost:3000/')
+      .then((response) => {
+        this.tableData = response.data.result
+      });
+    }
   }
 }
 </script>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import router from './router'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+Vue.prototype.$ajax = axios
 
 new Vue({
   render: h => h(App),
